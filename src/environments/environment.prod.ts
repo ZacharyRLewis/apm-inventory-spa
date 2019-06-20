6771db46f373d6b90744b37d2419ba775ccefd1f
Zach Lewis <zrlewis@winsupplyinc.com>
2019-06-20 11:07:47 -0400

    Adding prod and QA client ids and secrets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   env: 'prod',
   serviceUrl: 'https://agws1.winwholesale.com/apm-process-api',
-  mulesoftClientId: 'TODO',
-  mulesoftClientSecret: 'TODO'
+  mulesoftClientId: '2f7452772db4478188c698813864424b',
+  mulesoftClientSecret: '2B88af22Cf0342Ce9c1531e7C2546963'
 };
--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   env: 'qa',
   serviceUrl: 'https://qaagws1.winwholesale.com/apm-process-api',
-  mulesoftClientId: 'TODO',
-  mulesoftClientSecret: 'TODO'
+  mulesoftClientId: '5e8ef290f0ff43dab119b9dcf9a6d779',
+  mulesoftClientSecret: '6fa7347b3F2D465688AA2581baE93f82'
 };
