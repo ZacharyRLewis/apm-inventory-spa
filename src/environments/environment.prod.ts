4c8591a7b36665ff1e747b678a15b48b5e3a5164
Zach Lewis <zrlewis@winsupplyinc.com>
2019-06-26 10:00:49 -0400

    Updating QA/prod process api urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   env: 'prod',
-  serviceUrl: 'https://agws1.winwholesale.com/apm-process-api',
+  serviceUrl: 'https://agws1.winwholesale.com/api/apm-process-api',
   mulesoftClientId: '2f7452772db4478188c698813864424b',
   mulesoftClientSecret: '2B88af22Cf0342Ce9c1531e7C2546963'
 };
